7eec59141ace9275904024c485a98e016aad70de
Marat Elagin <maratelagin@gmail.com>
2021-12-26 11:29:10 +0300

    Add Login and Register modal windows

--- ClevInvest/wwwroot/js/site.js
+++ ClevInvest/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//password show
 
-// Write your JavaScript code.
+$('modal-body').on('click', '.password-control', function(){
+    if ($('.password').attr('type') == 'password'){
+        $(this).addClass('view');
+        $('.password').attr('type', 'text');
+    } else {
+        $(this).removeClass('view');
+        $('#password-input').attr('type', 'password');
+    }
+    return false;
+});
